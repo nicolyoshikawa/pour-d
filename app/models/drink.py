51ef07c7b300ef6558cdc838fe2998b7d4fc18dd
Huey Nguyen <syndrumablez@gmail.com>
2023-07-25 21:36:07 -0700

    Update drink model to include relationship with user

--- app/models/drink.py
+++ app/models/drink.py
@@ -9,19 +9,21 @@ class Drink(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    ABV = db.Column(db.Integer, nullable=False)
-    IBU = db.Column(db.Integer, nullable=False)
+    abv = db.Column(db.Integer, nullable=False)
+    ibu = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(255), nullable=False)
     drinkImgUrl = db.Column(db.String(255))
     createdAt = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     updatedAt = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
+    user = db.relationship('User', back_populates='drinks')
+
     def to_dict(self):
         return {
             'id': self.id,
             'userId': self.userId,
-            'ABV': self.ABV,
-            'IBU': self.IBU,
+            'abv': self.abv,
+            'ibu': self.ibu,
             'description': self.description,
             'drinkImgUrl': self.drinkImgUrl,
             'createdAt': self.createdAt.isoformat(),
