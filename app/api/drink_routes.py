0ae357256d8fe520410c72d9fdb68e05f1866ca3
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-07-29 00:30:41 -0700

    deleted dev.db from my local and added to .gitignore, added ids where needed

--- app/api/drink_routes.py
+++ app/api/drink_routes.py
@@ -1,18 +1,20 @@
 from flask import Blueprint, redirect, url_for, render_template
 from flask_login import login_required
-from app.models import User
+from app.models import User, Drink
 
 bp = Blueprint("drinks", __name__, url_prefix="/drinks")
 
 # Users can view all drinks.
 @bp.route("/", methods=["GET"])
 def getAllDrinks():
+    drinks = Drink.query.all()
     pass
 
 # Users can read a specific drink's information.
 @bp.route('/<int:id>', methods=["GET"])
-def getDrink():
-    pass
+def getDrink(id):
+    drink = Drink.query.get(id)
+    return drink.to_dict()
 
 # Logged in users can create a drink with ABV, IBU, description, and image.
 @bp.route('/', methods=["POST"])
@@ -23,13 +25,13 @@ def createADrink():
 # Logged in users can update a drink if they own it.
 @bp.route('/<int:id>', methods=["PUT"])
 @login_required
-def updateDrink():
+def updateDrink(id):
     pass
 
 # Logged in users can delete a drink if they own it.
 @bp.route('/<int:id>', methods=["DELETE"])
 @login_required
-def deleteDrink():
+def deleteDrink(id):
     pass
 
 # A logged in user can create a checkin/review for a drink.
--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -7,13 +7,13 @@ bp = Blueprint("reviews", __name__, url_prefix="/reviews")
 # A logged in user can update one of their own reviews.
 @bp.route("/<int:id>", methods=["PUT"])
 @login_required
-def updateReview():
+def updateReview(id):
     pass
 
 # A logged in user can delete one of their own reviews, removing it from the list of visible reviews without causing a refresh/redirect.
 @bp.route("/<int:id>", methods=["DELETE"])
 @login_required
-def deleteReview():
+def deleteReview(id):
     pass
 
 # A logged in user can read a list of checkins/reviews.
