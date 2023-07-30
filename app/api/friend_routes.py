b4489bd01547df8c543f7a77c355ffc12eea7cd3
Huey Nguyen <syndrumablez@gmail.com>
2023-07-29 17:02:02 -0700

    Updated friend routes to use composite PKs to find friendships

--- app/api/friend_routes.py
+++ app/api/friend_routes.py
@@ -4,44 +4,64 @@ from app.models import Friend, db
 
 friend_routes = Blueprint("friends", __name__)
 
-# A logged in user can add a friend.
+# A logged-in user can add a friend.
 @friend_routes.route("/request/<int:targetId>", methods=["POST"])
 @login_required
 def addFriend(targetId):
-    user = current_user.id
-    new_req = Friend(
-        user_id=user,
+    user_id = current_user.id
+    new_request = Friend(
+        user_id=user_id,
         friend_id=targetId,
         status="pending"
     )
-    db.session.add(new_req)
+    db.session.add(new_request)
     db.session.commit()
     return {"message": "Friend request sent"}
 
-# A logged in user can accept a friend request.
-@friend_routes.route("/accept/<int:requestId>", methods=["PUT"])
+# A logged-in user can accept a friend request.
+@friend_routes.route("/accept/<int:targetId>", methods=["PUT"])
 @login_required
-def acceptFriend(requestId):
-    request = Friend.query.get(requestId)
-    request.status = "friends"
+def acceptFriend(targetId):
+    user_id = current_user.id
+    friend_id = targetId
+    friendship = Friend.query.get((friend_id, user_id))
+
+    # Check if the friendship request exists
+    if not friendship:
+        return {"message": "Friend request not found"}, 404
+
+    friendship.status = "friends"
     db.session.commit()
     return {"message": "Request accepted"}
 
-# A logged in user can reject a friend request.
-@friend_routes.route("/reject/<int:requestId>", methods=["DELETE"])
+# A logged-in user can reject a friend request.
+@friend_routes.route("/reject/<int:targetId>", methods=["DELETE"])
 @login_required
-def rejectFriend(requestId):
-    request = Friend.query.get(requestId)
-    db.session.delete(request)
+def rejectFriend(targetId):
+    user_id = current_user.id
+    friend_id = targetId
+    friendship = Friend.query.get((friend_id, user_id))
+
+    # Check if the friendship request exists
+    if not friendship:
+        return {"message": "Friend request not found"}, 404
+
+    db.session.delete(friendship)
     db.session.commit()
     return {"message": "Request rejected"}
 
-# A logged in user can delete a friend.
+# A logged-in user can delete a friend.
 @friend_routes.route("/remove/<int:targetId>", methods=["DELETE"])
 @login_required
 def deleteFriend(targetId):
-    user = current_user.id
-    friend = Friend.query.all().filter(Friend.user_id == user and Friend.friend_id == targetId)
-    db.session.delete(friend)
+    user_id = current_user.id
+    friend_id = targetId
+    friendship = Friend.query.get((user_id, friend_id))
+
+    # Check if the friendship exists
+    if not friendship:
+        return {"message": "Friend not found"}, 404
+
+    db.session.delete(friendship)
     db.session.commit()
     return {"message": "Friend removed"}
