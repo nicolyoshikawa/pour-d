35f1483a45b0ec7a1ca5288323325588c1eef745
Huey Nguyen <syndrumablez@gmail.com>
2023-07-31 20:59:25 -0700

    Change drink_routes order for specificity

--- app/api/drink_routes.py
+++ app/api/drink_routes.py
@@ -7,14 +7,47 @@ from .auth_routes import validation_errors_to_error_messages
 
 drink_routes = Blueprint("drink", __name__)
 
+@drink_routes.route('/<int:id>/reviews', methods=["GET"])
+def getAllReviewsForADrink(id):
+    """
+    View all reviews for a drink
+    """
+    reviews = Review.query.filter(Review.drink_id == id).all()
+    if not reviews:
+        return {'errors': "Review could not be found"}, 404
+    return {'reviews': [review.to_dict() for review in reviews]}
 
-@drink_routes.route("/")
-def drinks():
+@drink_routes.route('/<int:id>/reviews', methods=["POST"])
+@login_required
+def createAReview(id):
     """
-    GET ALL DRINKS
+    A logged in user can create a checkin/review for a drink.
     """
-    drinks = Drink.query.all()
-    return {'drinks': [drink.to_dict() for drink in drinks]}
+    form = ReviewForm()
+    drink_id = id
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    drink = Drink.query.get(id)
+    if not drink:
+        return {'errors': "Drink could not be found"}, 404
+
+    # review = Review.query.filter(Review.drink_id == id, Review.user_id == current_user.id).first()
+    # if review:
+    #     return {'errors': "User already has a review for this drink"}, 403
+
+    if form.validate_on_submit():
+        review = Review(
+            content= form.data["content"],
+            stars= form.data["stars"],
+            user_id= current_user.id,
+            drink_id= drink_id,
+            review_img_url= form.data["review_img_url"]
+        )
+        db.session.add(review)
+        db.session.commit()
+
+        return review.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 @drink_routes.route("/<int:id>")
 def drink(id):
@@ -25,28 +58,6 @@ def drink(id):
     if not drink:
         return {'errors': "Drink could not be found"}, 404
     return drink.to_dict()
-
-@drink_routes.route("/", methods=["POST"])
-@login_required
-def new_drink():
-    """
-    CREATE A NEW DRINK
-    """
-    form = DrinkForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        drink = Drink(
-            name=form.data["name"],
-            abv=form.data["abv"],
-            ibu=form.data["ibu"],
-            description=form.data["description"],
-            drink_img_url=form.data["drink_img_url"],
-            user_id = current_user.id
-        )
-        db.session.add(drink)
-        db.session.commit()
-        return drink.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 @drink_routes.route("/<int:id>", methods=["PUT"])
 @login_required
@@ -88,53 +99,32 @@ def delete_drink(id):
         return {"message": "Drink successfully deleted"}
     return {'errors': ['Unauthorized']}
 
-@drink_routes.route('/<int:id>/reviews', methods=["POST"])
+@drink_routes.route("/")
+def drinks():
+    """
+    GET ALL DRINKS
+    """
+    drinks = Drink.query.all()
+    return {'drinks': [drink.to_dict() for drink in drinks]}
+
+@drink_routes.route("/", methods=["POST"])
 @login_required
-def createAReview(id):
+def new_drink():
     """
-    A logged in user can create a checkin/review for a drink.
+    CREATE A NEW DRINK
     """
-    form = ReviewForm()
-    drink_id = id
+    form = DrinkForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-
-    drink = Drink.query.get(id)
-    if not drink:
-        return {'errors': "Drink could not be found"}, 404
-
-    # review = Review.query.filter(Review.drink_id == id, Review.user_id == current_user.id).first()
-    # if review:
-    #     return {'errors': "User already has a review for this drink"}, 403
-
     if form.validate_on_submit():
-        review = Review(
-            content= form.data["content"],
-            stars= form.data["stars"],
-            user_id= current_user.id,
-            drink_id= drink_id,
-            review_img_url= form.data["review_img_url"]
+        drink = Drink(
+            name=form.data["name"],
+            abv=form.data["abv"],
+            ibu=form.data["ibu"],
+            description=form.data["description"],
+            drink_img_url=form.data["drink_img_url"],
+            user_id = current_user.id
         )
-        db.session.add(review)
+        db.session.add(drink)
         db.session.commit()
-
-        return review.to_dict()
+        return drink.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
-
-# View a review for a drink
-# @drink_routes.route('/<int:id>/reviews/<int:review_id>', methods=["GET"])
-# def getAReviewForADrink(id, review_id):
-    # review = Review.query.all().filter(Review.drink_id == id and Review.id == review_id)
-    # drink = Drink.query.get(id)
-    # if not review:
-    #     return {'errors': "Review could not be found"}, 404
-    # return review.to_dict()
-
-@drink_routes.route('/<int:id>/reviews', methods=["GET"])
-def getAllReviewsForADrink(id):
-    """
-    View all reviews for a drink
-    """
-    reviews = Review.query.filter(Review.drink_id == id).all()
-    if not reviews:
-        return {'errors': "Review could not be found"}, 404
-    return {'reviews': [review.to_dict() for review in reviews]}
