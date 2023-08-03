4b6ef8e3bef91b4845f231900700a40974a61ba8
Eric Kinder <ekind@Erics-MacBook-Air.local>
2023-08-03 00:54:31 -0400

    finished styling landing page

--- react-app/src/components/LandingPage/SearchBar.js
+++ react-app/src/components/LandingPage/SearchBar.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import "./LandingPage.css"
+
 export default function SearchBar() {
+    const [searchInput, setSearchInput] = useState("")
+    const history = useHistory()
+
+    const submitSearch = (e) => {
+        history.push("/")
+    }
     return (
-        <div>
-            
+        <div className="landing-search">
+            <form>
+                <input
+                    className="search-input"
+                    name="search"
+                    placeholder="Find a beer"
+                    onChange={(e) => setSearchInput(e)}
+                />
+                <button className="submit-search">
+                    SEARCH
+                </button>
+            </form>
         </div>
     )
 }
