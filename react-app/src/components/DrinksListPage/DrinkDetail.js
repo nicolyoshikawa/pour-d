dd009970bd97e9eb9393f5cfa5c296de10c004c7
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-08-02 22:02:48 -0700

    added isloaded

--- react-app/src/components/DrinksListPage/DrinkDetail.js
+++ react-app/src/components/DrinksListPage/DrinkDetail.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
 import { useParams } from 'react-router-dom';
 // import DrinkTile from './DrinkTile';
 import Reviews from "../Reviews";
