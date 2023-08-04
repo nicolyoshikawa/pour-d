cf667571825fcc94d6f73d41924be56480209d07
Eric Kinder <etkndr@gmail.com>
2023-08-03 23:53:37 -0400

    search bar functioning (no links currently)

--- react-app/src/components/LandingPage/SearchBar.js
+++ react-app/src/components/LandingPage/SearchBar.js
@@ -1,27 +1,55 @@
-import { useState, useEffect } from "react"
-import { useHistory } from "react-router-dom"
 import "./LandingPage.css"
+import { useState, useEffect } from "react"
+import { NavLink } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { loadAllDrinks } from "../../store/drinks"
 
 export default function SearchBar() {
-    const [searchInput, setSearchInput] = useState("")
-    const history = useHistory()
+    const dispatch = useDispatch()
 
-    const submitSearch = (e) => {
-        history.push("/")
+    const drinks = useSelector(state => state.drinks) // Get all drinks for search bar to query
+    const [searchInput, setSearchInput] = useState("")
+    let hideDiv
+
+    useEffect(() => {
+        dispatch(loadAllDrinks())
+    }, [dispatch])
+
+    
+    if (drinks) {
+        let filterDrinks = Object.values(drinks).filter((drink) => {
+            if (searchInput.length && drink.name.toLowerCase().includes(searchInput.toLowerCase())) {
+                return drink
+            }
+        })
+        
+        if (searchInput.length && filterDrinks.length) {
+            hideDiv = "results"
+        } else {
+            hideDiv = "results-hide"
+        }
+
+        return (
+            <div className="landing-search">
+                <form>
+                    <input
+                        className="search-input"
+                        placeholder="Find a beer"
+                        onChange={(e) => setSearchInput(e.target.value)}
+                    />
+                </form>
+                <div className={hideDiv}>
+                    {
+                        filterDrinks?.map((drink) => {
+                            return (
+                                <div className="result" key={drink.id}>
+                                    {drink.name}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
     }
-    return (
-        <div className="landing-search">
-            <form>
-                <input
-                    className="search-input"
-                    name="search"
-                    placeholder="Find a beer"
-                    onChange={(e) => setSearchInput(e)}
-                />
-                <button className="submit-search">
-                    SEARCH
-                </button>
-            </form>
-        </div>
-    )
 }
