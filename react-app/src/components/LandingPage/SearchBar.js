eb02e471e5489ad2d7ba62388c29c0976afee19e
Eric Kinder <etkndr@gmail.com>
2023-08-03 23:59:11 -0400

    search bar functioning (no links currently)

--- react-app/src/components/LandingPage/SearchBar.js
+++ react-app/src/components/LandingPage/SearchBar.js
@@ -1,15 +1,15 @@
 import "./LandingPage.css"
 import { useState, useEffect } from "react"
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom" // Links will be implemented after drink detail pages complete
 import { useDispatch, useSelector } from "react-redux"
 import { loadAllDrinks } from "../../store/drinks"
 
 export default function SearchBar() {
     const dispatch = useDispatch()
 
-    const drinks = useSelector(state => state.drinks) // Get all drinks for search bar to query
-    const [searchInput, setSearchInput] = useState("")
-    let hideDiv
+    const drinks = useSelector(state => state.drinks) // Load all drinks for search bar to query
+    const [searchInput, setSearchInput] = useState("") // Set current query to user's input
+    let hideDiv // Used for removing border if search results are empty
 
     useEffect(() => {
         dispatch(loadAllDrinks())
@@ -17,12 +17,14 @@ export default function SearchBar() {
 
     
     if (drinks) {
+        // Create array of drinks with name matching current input from user
         let filterDrinks = Object.values(drinks).filter((drink) => {
             if (searchInput.length && drink.name.toLowerCase().includes(searchInput.toLowerCase())) {
                 return drink
             }
         })
         
+        // Remove border if results are empty
         if (searchInput.length && filterDrinks.length) {
             hideDiv = "results"
         } else {
