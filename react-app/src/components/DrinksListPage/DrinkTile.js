373be0b80bf341c134dcb2c2de0194f27891ec35
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-08-05 15:09:22 -0700

    added stars and fixed review stars

--- react-app/src/components/DrinksListPage/DrinkTile.js
+++ react-app/src/components/DrinksListPage/DrinkTile.js
@@ -5,7 +5,17 @@ const DrinkTile = ({drink, clickable}) => {
     const date = new Date(drink.created_at)
     const dateFormat = date.toLocaleDateString()
     const avgRating = drink.review_avg ? Number(drink.review_avg).toFixed(2) : "New";
+    const star = <i className="fa-solid fa-star"></i>
+    const emptyStar = <i className="fa-regular fa-star"></i>
 
+    // Calculate full and empty stars to match review rating
+    let makeRating = []
+    for (let i = 0; i < avgRating; i++) {
+        makeRating.push(1)
+    }
+    while (makeRating.length < 5){
+        makeRating.push(0)
+    }
     return (
         <div className="DrinkTile">
             <div className="drinkImg">
@@ -21,7 +31,15 @@ const DrinkTile = ({drink, clickable}) => {
             <div className="drinkDetails">
                 <div>{drink.abv}% ABV</div>
                 <div className="leftBorder">{drink.ibu} IBU</div>
-                <div className="leftBorder">STARS ({avgRating})</div>
+                <div className="leftBorder">
+                    {makeRating?.map((rating, el) => {
+                        if (rating === 1) {
+                            return <span key={el} className="star">{star}</span>
+                        } else {
+                           return <span key={el} className="star">{emptyStar}</span>
+                        }
+                    })} ({avgRating})
+                </div>
                 <div className="leftBorder">{drink.review_count} Ratings</div>
                 {clickable ? (
                     <div className="leftBorder">Added {dateFormat}</div>
