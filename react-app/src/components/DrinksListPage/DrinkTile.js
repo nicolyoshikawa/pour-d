88ee85f008e3a50fea8acab13309230d0a43dd17
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-08-05 10:29:07 -0700

    editing drink list css

--- react-app/src/components/DrinksListPage/DrinkTile.js
+++ react-app/src/components/DrinksListPage/DrinkTile.js
@@ -2,23 +2,27 @@ import { Link } from "react-router-dom";
 import "./Drinks.css";
 
 const DrinkTile = ({drink, clickable}) => {
+    const date = new Date(drink.created_at)
+    const dateFormat = date.toLocaleDateString()
     return (
         <div className="DrinkTile">
-            <div>
-                <img key={drink.id} src={`${drink?.drink_img_url}`} alt={drink.description} title={drink.name} />
+            <div className="drinkGrid">
+                <div className="drinkImg">
+                    <img key={drink.id} src={`${drink?.drink_img_url}`} alt={drink.description} title={drink.name} />
+                </div>
+                {clickable ? (
+                    <div className="drinkName"><Link to={`/drinks/${drink.id}`}> {drink.name}</Link></div>
+                    ) : <div>{drink.name}</div>
+                }
+                <div className="drinkDescription">{drink.description}</div>
             </div>
-            {clickable ? (
-                <div><Link to={`/drinks/${drink.id}`}> {drink.name}</Link></div>
-                ) : <div>{drink.name}</div>
-            }
-            <div>{drink.description}</div>
             <div className="drinkDetails">
                 <div>{drink.abv}% ABV</div>
                 <div className="leftBorder">{drink.ibu} IBU</div>
                 <div className="leftBorder">stars</div>
                 <div className="leftBorder">number of ratings</div>
                 {clickable ? (
-                    <div className="leftBorder">Added {drink.created_at}</div>
+                    <div className="leftBorder">Added {dateFormat}</div>
                     ) : <div></div>
                 }
             </div>
