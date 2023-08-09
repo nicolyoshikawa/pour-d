7b67ae59555cbe8e32594a1b2c3240abdbc0eee5
Huey Nguyen <syndrumablez@gmail.com>
2023-08-08 21:57:28 -0700

    Begin react component

---
+++ react-app/src/components/FriendsPage/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import "./FriendsPage.css"
+
+const FriendsPage = () => {
+    
+  return (
+    <div>
+      Friends page here
+    </div>
+  )
+};
+
+export default FriendsPage;
