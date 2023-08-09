b2309e161a5f372fefecf30d4d995b027826c740
Huey Nguyen <syndrumablez@gmail.com>
2023-08-08 22:10:29 -0700

    WIP friends

--- react-app/src/components/FriendsPage/index.js
+++ react-app/src/components/FriendsPage/index.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
+
+import * as userActions from "../../store/currUser"
 import "./FriendsPage.css"
 
 const FriendsPage = () => {
+    const dispatch = useDispatch();
+    const friends = useSelector(state => state.currUser.friends)
+
+    useEffect(() => {
+        dispatch(userActions.getUserFriends())
+    }, [dispatch])
     
+
   return (
     <div>
       Friends page here
