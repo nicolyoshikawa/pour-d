f56c745d4de86f808e032813829567a9f78680af
Eric Kinder <etkndr@gmail.com>
2023-08-09 23:27:55 -0400

    removed user dispatch

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -4,18 +4,15 @@ import { useDispatch, useSelector } from "react-redux";
 import Review from "../Review";
 import { loadAllReviews } from "../../store/reviews";
 import { loadAllDrinks } from "../../store/drinks";
-import { loadUsers } from "../../store/users";
 
 export default function Home() {
     const dispatch = useDispatch()
     const reviews = useSelector(state => Object.values(state.reviews))
     const drinks = useSelector(state => state.drinks)
-    const users = useSelector(state => state.users)
 
     useEffect(() => {
         dispatch(loadAllReviews())
         dispatch(loadAllDrinks())
-        dispatch(loadUsers())
     }, [dispatch])
 
     return (
