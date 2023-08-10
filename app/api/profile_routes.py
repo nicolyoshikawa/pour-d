fe3f8a355546540d645f139126dfb42e8f8502f1
Huey Nguyen <syndrumablez@gmail.com>
2023-08-09 21:14:33 -0700

    Update backend route to return friends' info

--- app/api/profile_routes.py
+++ app/api/profile_routes.py
@@ -46,11 +46,26 @@ def getCurrentUserFriends():
     """
     A logged in user can view all friends.
     """
-    find_friends = Friend.query.all()
-    all_friends = [request.to_dict() for request in find_friends if (request.friend_id == current_user.id or request.user_id == current_user.id) and request.status == "friends"]
-    user_ids = [friend["user_id"] for friend in all_friends if friend["user_id"] != current_user.id]
-    friend_ids = [friend["friend_id"] for friend in all_friends if friend["friend_id"] != current_user.id]
-    return user_ids + friend_ids
+    # find_friends = Friend.query.all()
+    # all_friends = [request.to_dict() for request in find_friends if (request.friend_id == current_user.id or request.user_id == current_user.id) and request.status == "friends"]
+    # user_ids = [friend["user_id"] for friend in all_friends if friend["user_id"] != current_user.id]
+    # friend_ids = [friend["friend_id"] for friend in all_friends if friend["friend_id"] != current_user.id]
+    # return user_ids + friend_ids
+
+
+    confirmed_friends = Friend.query.filter(
+        ((Friend.friend_id == current_user.id) | (Friend.user_id == current_user.id)) &
+        (Friend.status == "friends")
+    ).all()
+
+    friends_info = []
+    for friend_request in confirmed_friends:
+        if friend_request.user_id != current_user.id:
+            friends_info.append(friend_request.user.to_dict())
+        elif friend_request.friend_id != current_user.id:
+            friends_info.append(friend_request.friend.to_dict())
+
+    return jsonify(friends_info)
 
 
 @profile_routes.route("/friend-activity", methods=["GET"])
