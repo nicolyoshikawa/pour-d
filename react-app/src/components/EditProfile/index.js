7a69be2e5af4c10f759dc38ad7ecc332268fd8be
Huey Nguyen <syndrumablez@gmail.com>
2023-08-10 20:53:12 -0700

    Edit profile text -> Page coming soon!

---
+++ react-app/src/components/EditProfile/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import "./EditProfile.css";
+
+const EditProfile = () => {
+  return (
+    <div className="edit-profile-container">
+      Edit Profile coming soon!
+    </div>
+  )
+};
+
+export default EditProfile;
