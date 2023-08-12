7c83ec0aefa04964ce2d400988f103324086ee99
Huey Nguyen <syndrumablez@gmail.com>
2023-08-12 11:09:13 -0700

    User1 has rel with every other user

--- app/seeds/friends.py
+++ app/seeds/friends.py
@@ -13,20 +13,31 @@ def seed_friends():
     user9 = User.query.get(9)
     user10 = User.query.get(10)
 
+
+    # User1 (Demo User Seed) #######
     # User1 is friends with User2
     friend1 = Friend(user=user1, friend=user2, status='friends')
     # User1 has pending friend request to User3
     friend2 = Friend(user=user1, friend=user3, status='pending')
-    # User2 has pending friend request to User3
-    friend3 = Friend(user=user2, friend=user3, status='pending')
     # User1 is friends with User4
     friend4 = Friend(user=user1, friend=user4, status='friends')
+    # User1 is friends with User5
+    friend7 = Friend(user=user1, friend=user5, status='friends')
+    # User1 has pending friend request to User7
+    friend12 = Friend(user=user1, friend=user7, status='pending')
+    # User8 has pending friend request to User1
+    friend15 = Friend(user=user8, friend=user1, status='pending')
+    # User9 has pending friend request to User1
+    friend16 = Friend(user=user9, friend=user1, status='pending')
+    # User6 has pending friend request to User1
+    friend17 = Friend(user=user6, friend=user1, status='pending')
+    
+    # User2 has pending friend request to User3
+    friend3 = Friend(user=user2, friend=user3, status='pending')
     # User2 is friends with User4
     friend5 = Friend(user=user2, friend=user4, status='friends')
     # User3 has pending friend request to User4
     friend6 = Friend(user=user3, friend=user4, status='pending')
-    # User1 is friends with User5
-    friend7 = Friend(user=user1, friend=user5, status='friends')
     # User4 has pending friend request to User5
     friend8 = Friend(user=user4, friend=user5, status='pending')
     # User2 is friends with User6
@@ -35,16 +46,11 @@ def seed_friends():
     friend10 = Friend(user=user5, friend=user6, status='pending')
     # User3 is friends with User7
     friend11 = Friend(user=user3, friend=user7, status='friends')
-    # User1 has pending friend request to User7
-    friend12 = Friend(user=user1, friend=user7, status='pending')
     # User8 is friends with User9
     friend13 = Friend(user=user8, friend=user9, status='friends')
     # User9 has pending friend request to User10
     friend14 = Friend(user=user9, friend=user10, status='pending')
-    # User8 has pending friend request to User1
-    friend15 = Friend(user=user8, friend=user1, status='pending')
-    # User9 has pending friend request to User1
-    friend16 = Friend(user=user9, friend=user1, status='pending')
+
     
     db.session.add(friend1)
     db.session.add(friend2)
@@ -62,6 +68,7 @@ def seed_friends():
     db.session.add(friend14)
     db.session.add(friend15)
     db.session.add(friend16)
+    db.session.add(friend17)
 
     db.session.commit()
 
